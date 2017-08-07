c2b1d81a2468150d25dbfa0fd4ea7a7aa8e9cd0a
psanchezp <psanchezp94@gmail.com>
2017-08-07 18:47:13 -0500

    Puma, ruby version, active record

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
+
+ruby '2.3.4'
+
 gem 'slim'
 gem 'sinatra'
--- notes.rb
+++ notes.rb
@@ -1,6 +1,10 @@
 # Notes.rb
 require 'sinatra'
 require 'slim'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 
 get '/' do
   slim :dashboard
